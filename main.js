2ca10fef20a0e434da0f325d96c5d713c8bdc879
Jollen Chen <jollen@moko365.com>
2013-10-20 10:27:59 +0800

    fix #9

--- main.js
+++ main.js
@@ -1,37 +1,55 @@
 var gameModule = (function() {
     
     var timeoutVar,
-    counter = 0;
+    counter = 0;  
+    
+    var colors = ['pink','yellow','green']
+    var length = colors.length;
+    
+    function touchEven(evt){
+
+             var x = evt.clientX,
+                 y = evt.clientY;
+
+            console.log("click: "+ x +","+ y);
+   } 
 
     function start(){
+             document.getElementById("main").addEventLintener("click",touchEven,false);  
+             startGame();
+         }
 
+    function startGame(){
         var canvas = document.getElementById('game');
         var ctx = canvas.getContext('2d');
 
-        var ballX = Math.floor(Math.random()*300);
-        var ballY = Math.floor(Math.random()*500);
-        var ballR = Math.floor(Math.random()*100);
+        var ballX = Math.floor(Math.random()*600);
+        var ballY = Math.floor(Math.random()*450);
+        var ballR = Math.floor(Math.random()*80);
 
-        canvas.width = 480;
-        canvas.height = 320;
+        canvas.width = 640;
+        canvas.height = 480;
 
-        ctx.fillStyle = 'pink';
+        ctx.fillStyle = colors[counter % length];
         ctx.beginPath();
         ctx.arc(ballX, ballY, ballR, 0, Math.PI * 2 , true);
         ctx.fill();
 
 
-        if (counter >= 50 ) {
+        if (counter >= 20 ) {
+            gameOver();
 
         }else{
-         timeoutVar =setTimeout(start,500)
+         timeoutVar =setTimeout(start,2000)
         counter = counter + 1;
 
-        console.log("counter:"+counter);
         }
 
       }
-      
+    function gameOver(){
+        console.log("counter:"+counter);
+    }
+
 
       return{
         start:start
