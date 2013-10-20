83e47095649e7a8c0fb4a14bbab2e25f975d2685
Jollen Chen <jollen@moko365.com>
2013-10-20 12:08:00 +0800

    fix #10

--- main.js
+++ main.js
@@ -1,41 +1,26 @@
-var gameModule = (function() {
+/*jslint browser: true, devel: true, closure: true */
+var gameModule = (function (document) {
+    
+    "use strict";
+    var counter = 0,
+        ballX,
+        ballY,
+        ballR,
+        scores,
+        colors = ['pink', 'green', 'yellow'],
+        length = colors.length;
 
-        var timeoutVar,
-                counter = 0,
-                ballX,
-                ballY,
-                ballR,
-                scores,
-                colors = ['pink', 'green', 'yellow'],
-                length = colors.length;
-
-        function touchEvent(evt) {
-                var x = evt.clientX,
-                        y = evt.clientY,
-                        tmp = (ballX - x) * (ballX - x) + (ballY - y) * (ballY - y);
-
-                console.log("Clicked: " + x + " , " + y);
-
-                if (tmp < ballR*ballR) {
-                        scores = scores + (100 - ballR);
-                        console.log("Hit ! Your scores: " + scores);
-                }
+    function gameOver() {
+            console.log("Final: " + scores);
         }
 
-        function start() {
-                scores = 0;
-
-                document.getElementById("main").addEventListener("click", touchEvent, false);
-                startGame();                
-        }
-
-        function startGame() {
+    function startGame() {
         var canvas = document.getElementById('game');
-        var ctx = canvas.getContext('2d');
+            ctx = canvas.getContext('2d');
             
-            ballX = Math.floor(Math.random() * 600); // 0..300
-            ballY = Math.floor(Math.random() * 450);
-            ballR = Math.floor(Math.random() * 80);
+        ballX = Math.floor(Math.random() * 600); // 0..300
+        ballY = Math.floor(Math.random() * 450);
+        ballR = Math.floor(Math.random() * 80);
 
         canvas.width = 640;
         canvas.height = 480;
@@ -48,18 +33,34 @@ var gameModule = (function() {
         if (counter >= 10) {
                 gameOver();
         } else {
-                timeoutVar = setTimeout(startGame, 2000);
+            setTimeout(startGame, 2000);
                 counter = counter + 1;
             } 
         }
 
-        function gameOver() {
-        console.log("Final: " + scores);
+        function touchEvent(evt){
+            var x = evt.clientX,
+                y = evt.clientY,
+                tmp = (ballX - x) * (ballX - x) + (ballY - y) * (ballY - y);
+
+                console.log("Clicked: " + x + " , " + y);
+
+                if (tmp < ballR * ballR) {
+                    scores = scores + (100 - ballR);
+                    console.log("Hit ! Your scores: " + scores);
+                }
+        }
+
+        function start() {
+                scores = 0;
+
+                document.getElementById("main").addEventListener("click", touchEvent, false);
+                startGame();                
         }
 
         return {
-                start: start
-        }
-}) ();
+            start: start
+        };
+}(document));
 
 gameModule.start();
