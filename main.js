59962ceda30c54bf4f815d71ccf83876bd5b9c50
Jollen Chen <jollen@moko365.com>
2013-10-20 10:26:08 +0800

    fix #9

--- main.js
+++ main.js
@@ -31,34 +31,7 @@ var gameModule = (function() {
         }
 
       }
-      function start(){
-
-        var canvas = document.getElementById('game');
-        var ctx = canvas.getContext('2d');
-
-        var ballX = Math.floor(Math.random()*300);
-        var ballY = Math.floor(Math.random()*500);
-        var ballR = Math.floor(Math.random()*100);
-
-        canvas.width = 480;
-        canvas.height = 320;
-
-        ctx.fillStyle = 'black';
-        ctx.beginPath();
-        ctx.arc(ballX, ballY, ballR, 0, Math.PI * 2 , true);
-        ctx.fill();
-
-
-        if (counter >= 50 ) {
-
-        }else{
-         timeoutVar =setTimeout(start,500)
-        counter = counter + 1;
-
-        console.log("counter:"+counter);
-        }
-
-      }
+      
 
       return{
         start:start
